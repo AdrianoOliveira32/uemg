91957ae65f5e8507ccdffacffac7b7eb3629db97
promailson <promailson@gmail.com>
2017-08-08 11:47:58 -0300

    Update main.cpp

--- Sistemas de Informação/ED1/Codigos/ContarPalavrasEmString/main.cpp
+++ Sistemas de Informação/ED1/Codigos/ContarPalavrasEmString/main.cpp
@@ -7,7 +7,7 @@ int main()
 	string palavra = "nao";
 	string frase = "palmeiras nao tem mundial naonao";
 	int cont = 0;
-    int pos = 0;
+        int pos = 0;
 
 	while ((pos = frase.find(palavra, pos)) < frase.length())
 	{
