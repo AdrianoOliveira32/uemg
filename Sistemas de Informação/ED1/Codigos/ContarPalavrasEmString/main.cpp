fef1ee8c52430b81b82b11039bd9fda1ed7080e7
Mailson Queiroz <promailson@gmail.com>
2017-08-08 11:45:45 -0300

    Exemplo contar palavra em frase

---
+++ Sistemas de Informação/ED1/Codigos/ContarPalavrasEmString/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string palavra = "nao";
+	string frase = "palmeiras nao tem mundial naonao";
+	int cont = 0;
+    int pos = 0;
+
+	while ((pos = frase.find(palavra, pos)) < frase.length())
+	{
+		cont++;
+		pos += palavra.length();
+	}
+	cout << cont;
+	return 0;
+}
